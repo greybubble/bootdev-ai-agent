b4e558d9483ca6f25ed7afab7d82c96047abdf1d
greybubble <greybubble@gmail.com>
2025-09-12 11:14:53 -0500

    implemented genai types

--- main.py
+++ main.py
@@ -1,22 +1,35 @@
 import os
+import sys
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 client = genai.Client(api_key=api_key)
 
-query = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
 
 def main():
+    if len(sys.argv) < 2:
+        print("Prompt was not provided")
+        exit(code=1)
+    
+    query = sys.argv[1]
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=query)])
+    ]
+    response = client.models.generate_content(model="gemini-2.0-flash-001", contents=messages)
+
+
     print("Hello from bootdev-ai-agent!")
     print()
     print(f"Prompt: {query}")
-
-    response = client.models.generate_content(model="gemini-2.0-flash-001", contents=query)
+    print()
     print(response.text)
     print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
     print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+
+
 
 if __name__ == "__main__":
     main()
