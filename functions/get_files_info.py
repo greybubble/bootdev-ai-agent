ae6115364f55f586732f78d97b2aea5c215f2ad5
greybubble <greybubble@gmail.com>
2025-09-14 18:52:38 -0500

    updates get_file_info and tests to matchbootdev expectations

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -5,26 +5,21 @@ def get_files_info(working_directory, directory="."):
     target_path = os.path.join(working_directory, directory)
     target_path = os.path.abspath(target_path)
  
-    title = ""
-
-    if directory == ".":
-        title = f"Result for current directory:"
-    else:
-        title = f"Result for '{directory}' directory:"
-
     if not target_path.startswith(wd_path):
-        return '\n'.join([title, f'    Error: Cannot list "{directory}" as it is outside the permitted working directory'])
+        return f'    Error: Cannot list "{directory}" as it is outside the permitted working directory'
     
     if not os.path.isdir(target_path):
-        return '\n'.join([title, f'    Error: "{directory}" is not a directory'])
+        return f'    Error: "{directory}" is not a directory'
     
-    contents = os.listdir(target_path)
-    result = [title]
+    try:
+        contents = os.listdir(target_path)
+        result = []
 
-    for file in contents:
-        if file == "__pycache__":
-            continue
-        path_to_file = os.path.join(target_path, file)
-        result.append(f" - {file}: file_size={os.path.getsize(path_to_file)} bytes, is_dir={os.path.isdir(path_to_file)}")
-
-    return "\n".join(result)
+        for file in contents:
+            path_to_file = os.path.join(target_path, file)
+            result.append(f" - {file}: file_size={os.path.getsize(path_to_file)} bytes, is_dir={os.path.isdir(path_to_file)}")
+        return "\n".join(result)
+    
+    except Exception as e:
+        return f"Error listing files: {e}"
+    
--- tests.py
+++ tests.py
@@ -8,38 +8,27 @@ class TestGetFilesInfo(unittest.TestCase):
 
     def test_wd(self):
         result = self.get_files_info("calculator", ".")
-        expected = f'''Result for current directory:
- - main.py: file_size={os.path.getsize("./calculator/main.py")} bytes, is_dir=False
- - tests.py: file_size={os.path.getsize("./calculator/tests.py")} bytes, is_dir=False
- - pkg: file_size={os.path.getsize("./calculator/pkg")} bytes, is_dir=True'''
+        print(f"Result for current directory:")
         print(result)
         
-        self.assertEqual(result, expected)
 
     def test_pkg(self):
         result = self.get_files_info("calculator", "pkg")
-        expected = f'''Result for 'pkg' directory:
- - render.py: file_size={os.path.getsize("./calculator/pkg/render.py")} bytes, is_dir=False
- - calculator.py: file_size={os.path.getsize("./calculator/pkg/calculator.py")} bytes, is_dir=False'''
+        print(f"Result for 'pkg' directory:")
         print(result)
 
-        self.assertEqual(result, expected)
 
     def test_bin(self):
         result = self.get_files_info("calculator", "/bin")
-        expected = '''Result for '/bin' directory:
-    Error: Cannot list "/bin" as it is outside the permitted working directory'''
+        print("Result for '/bin' directory:")
         print(result)
 
-        self.assertEqual(result, expected)
 
     def test_pd(self):
         result = self.get_files_info("calculator", "../")
-        expected = '''Result for '../' directory:
-    Error: Cannot list "../" as it is outside the permitted working directory'''
+        print("Result for '../' directory:")
         print(result)
 
-        self.assertEqual(result, expected)
 
 
 if __name__ == "__main__":
